3aaade9d06363a7b4921771fb1cd40773cd28041
Wlodek Bzyl <matwb@univ.gda.pl>
2017-09-26 18:33:27 +0200

    Update

---
+++ Ruby/keyword_arguments.rb
@@ -0,0 +1,24 @@
+# Ian C. Anderson, Ruby 2 Keyword Arguments
+
+def foo(bar: 'default')
+  puts bar
+end
+
+foo # => 'default'
+foo(bar: 'baz') # => 'baz'
+
+# Required keyword arguments
+
+def koo(bar:)
+  puts bar
+end
+
+# koo # => ArgumentError: missing keyword: bar
+koo(bar: 'baz') # => 'baz'
+
+def obvious_total(subtotal:, tax:, discount:)
+  subtotal + tax - discount
+end
+
+puts obvious_total(subtotal: 100, tax: 10, discount: 5) # => 105
+puts obvious_total(subtotal: 100, discount: 5, tax: 10) # => 105
