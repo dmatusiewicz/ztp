3546a05c0e83a532849efaf12e7388fcbfb797ba
Wlodek Bzyl <matwb@univ.gda.pl>
2017-10-08 17:18:47 +0200

    update

--- Ruby/code_smells.rb
+++ Ruby/code_smells.rb
@@ -4,9 +4,9 @@ require 'prawn'
 require 'prawn/table'
 
 require 'csv'
-require 'pp'
 
 Prawn::Document.generate('table.pdf') do
+  # puts "Prawn:DATADIR: #{Prawn::DATADIR}"
   font '/Users/wbzyl/.fonts/Roboto/Roboto-Regular.ttf'
   font_size 11
 
@@ -20,23 +20,10 @@ Prawn::Document.generate('table.pdf') do
   csv_data = open(csv_file).readlines
   body = CSV.parse(csv_data.join)
 
-  cs = make_cell content: 'Code Smell'
-  counter = make_cell content: 'Counter'
-
-  index = 0
-  counted = body.map do |row|
-    index += 1
-    row.unshift (index < 10 ? ' ' : '') + index.to_s
-    row += [' ', ' ']
-  end
-
-  counted.unshift [cs, counter]
-
   table(
-    counted,
+    body,
     header: true,
     width: 540,
-    column_widths: { 0 => 32 },
     cell_style: {
       height: 36, padding: [12, 6, 12, 6], overflow: :shrink_to_fit
     }
@@ -44,4 +31,3 @@ Prawn::Document.generate('table.pdf') do
 end
 
 puts '--> table.pdf'
-# puts "Prawn:DATADIR: #{Prawn::DATADIR}"
