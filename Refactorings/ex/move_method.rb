bca95c766d443bf04d0d657496e1e5947b91406a
Wlodek Bzyl <matwb@univ.gda.pl>
2017-10-20 21:28:06 +0200

    Update

--- Refactorings/ex/move_method.rb
+++ Refactorings/ex/move_method.rb
@@ -1,3 +1,17 @@
+# There are several account types. Each has its own rule
+# for calculating overdraft charge.
+class AccountPremium
+  def premium?
+    true
+  end
+end
+
+puts AccountPremium.new.premium?
+
+# class AccountStandard
+# end
+
+
 # Look through methods on a class to find a method that seems to reference
 # another object more than the object it lives on.
 class Account
@@ -23,29 +37,18 @@ class Account
   end
 end
 
-# There are several account types. Each has its own rule
-# for calculating overdraft charge.
-class AccountType
-  attr_accessor :account_type
-  def initialize
-    @account_type = :standard
-  end
+my_account = Account.new(10, AccountPremium.new)
+puts my_account.overdraft_charge
+puts my_account.bank_charge
 
-  def premium?
-    account_type == :premium
-  end
-end
 
-premium_account = AccountType.new
-premium_account.account_type = :premium
-premium_account.premium?
+# Move the overdraft_charge method in the AcountPremium class.
 
-# Move the overdraft_charge method
 
 # Step 1 & 2
-# overdraft_charge method uses @days_overdrawn; it should stay at Account class
+# the overdraft_charge method uses @days_overdrawn; it should stay at Account class
 
-class AccountType
+class AccountPremium
   def overdraft_charge(days_overdrawn)
     if premium?
       result = 10
@@ -56,6 +59,13 @@ class AccountType
     end
   end
 end
+
+# my_account = Account.new(10, AccountPremium.new)
+puts my_account.overdraft_charge
+puts my_account.bank_charge
+
+
+# Step 5 (delegation)
 
 class Account
   def overdraft_charge
@@ -71,9 +81,6 @@ class Account
   end
 end
 
-my_account = Account.new(10, premium_account)
-my_account.overdraft_charge
-my_account.bank_charge
-
-
-#
+# my_account = Account.new(10, AccountPremium.new)
+puts my_account.overdraft_charge
+puts my_account.bank_charge
