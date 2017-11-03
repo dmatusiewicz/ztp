0a9c1687fd87442588e3784fa145857b75f33ef4
Wlodek Bzyl <matwb@univ.gda.pl>
2017-11-03 17:16:41 +0100

    Update

--- Refactorings/ex/simplifying_conditional_expressions-04.rb
+++ Refactorings/ex/simplifying_conditional_expressions-04.rb
@@ -25,11 +25,51 @@ an_account.adjusted_capital
 
 # Step 1
 
+def adjusted_capital
+  result = 0.0
+  return result if @capital <= 0.0               # reversed conditional 1
+  if @interest_rate > 0.0 && @duration > 0.0
+    result = (@income / @duration) * ADJ_FACTOR
+  end
+  result
+end
+
 # Step 2
+
+def adjusted_capital
+  result = 0.0
+  return result if @capital <= 0.0
+  return result if !(@interest_rate > 0.0 && @duration > 0.0) # reversed conditional 2
+  result = (@income / @duration) * ADJ_FACTOR
+  result
+end
 
 # Step 3
 
-# Step 4
+def adjusted_capital
+  result = 0.0
+  return result if @capital <= 0.0
+  return result if @interest_rate <= 0.0 || @duration <= 0.0 # simplified conditional 2
+  result = (@income / @duration) * ADJ_FACTOR
+  result
+end
 
+# Step 4 – use explicit value
+
+def adjusted_capital
+  result = 0.0
+  return 0 if @capital <= 0.0
+  return 0 if @interest_rate <= 0.0 || @duration <= 0.0
+  result = (@income / @duration) * ADJ_FACTOR
+  result
+end
+
+# Step 5
+
+def adjusted_capital
+  return 0.0 if @capital <= 0.0
+  return 0.0 if @interest_rate <= 0.0 || @duration <= 0.0
+  (@income / @duration) * ADJ_FACTOR
+end
 
 # reek simplifying_conditional_expressions-04.rb
