14ce640207f9e6197f571c349ee065f0662d2435
Wlodek Bzyl <matwb@univ.gda.pl>
2017-11-03 16:57:44 +0100

    add simplifying_conditional_expressions-03.rb

--- Refactorings/ex/simplifying_conditional_expressions-03.rb
+++ Refactorings/ex/simplifying_conditional_expressions-03.rb
@@ -1,41 +1,96 @@
 # Smell: Simplifying Conditional Expressions
 
 # Refactoring: Replace Nested Conditional with Guard Clauses
-class PZU
-  def initialize(seniority, months_disabled, is_part_time)
-    @seniority = seniority
-    @months_disabled = months_disabled
-    @is_part_time = is_part_time
+class OC
+  def initialize(dead, separated, retired)
+    @dead = dead
+    @separated = separated
+    @retired = retired
   end
 
-  def disability_amount
-    return 0 if @seniority < 2
-    return 0 if @months_disabled > 12
-    return 0 if @is_part_time
+  def pay_amount
+    if @dead
+      result = dead_amount
+    else
+      if @separated
+        result = separated_amount
+      else
+        if @retired
+          result = retired_amount
+        else
+          result = normal_pay_amount
+        end
+      end
+    end
+    result
+  end
 
-    Math::sin(@seniority * @months_disabled).abs
+  private
+
+  def dead_amount
+    100_000
+  end
+
+  def separated_amount
+    10_000
+  end
+
+  def normal_pay_amount
+    30_000
   end
 end
 
-a_pzu = PZU.new 3, 6, false
-a_pzu.disability_amount
+a_oc = OC.new true, false, false
+a_oc.pay_amount
 
-# Replace Nested Conditional with Guard Clauses (final?)
-class PZU
-  def initialize(seniority, months_disabled, is_part_time)
-    @seniority = seniority
-    @months_disabled = months_disabled
-    @is_part_time = is_part_time
-  end
+# Replace Nested Conditional with Guard Clauses (steps)
 
-  def disability_amount
-    return 0 if @seniority < 2 || @months_disabled > 12 || @is_part_time
-
-    Math::sin(@seniority * @months_disabled).abs
+# Step 1
+def pay_amount
+  return dead_amount if @dead
+  if @separated
+    result = separated_amount
+  else
+    if @retired
+      result = retired_amount
+    else
+      result = normal_pay_amount
+    end
+    result
   end
 end
 
-a_pzu = PZU.new 3, 6, false
-a_pzu.disability_amount
+# Step 2
 
-# reek simplifying_conditional_expressions-01.rb
+def pay_amount
+  return dead_amount if @dead
+  return separated_amount if @separated
+  if @retired
+    result = retired_amount
+  else
+    result = normal_pay_amount
+  end
+  result
+end
+
+# Step 3
+
+def pay_amount
+  return dead_amount if @dead
+  return separated_amount if @separated
+  return retired_amount if @retired
+  result = normal_pay_amount
+  result
+end
+
+# Step 4
+
+def pay_amount
+  return dead_amount if @dead
+  return separated_amount if @separated
+  return retired_amount if @retired
+  normal_pay_amount
+end
+
+
+# reek simplifying_conditional_expressions-03.rb
